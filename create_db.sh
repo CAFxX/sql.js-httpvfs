7f9fe4b401c0c3e137e509637cd1a3decef9629f
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-08-11 11:04:50 +0900

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -11,23 +11,23 @@ find "$outdir" -type f -name 'db.sqlite3.*' -delete
 log(){ local x=$1 n=2 l=-1;if [ "$2" != "" ];then n=$x;x=$2;fi;while((x));do let l+=1 x/=n;done;echo $l; }
 
 # for chunked mode, we need to know the database size in bytes beforehand
-bytes="$(stat --printf="%s" "$indb")"
+bytes="$( stat --printf="%s" "$indb" )"
 # set request chunk size to match page size
-pageSize="$(sqlite3 "$indb" 'pragma page_size')"
+pageSize="$( sqlite3 "$indb" 'pragma page_size' )"
 requestChunkSize=$pageSize
 # set chunk size to a multiple of the `pragma page_size`)
 pagesPerServerChunk=1
-serverChunkSize=$(($pagesPerServerChunk * $pageSize))
+serverChunkSize=$(( $pagesPerServerChunk * $pageSize ))
 # calculate suffix size
-pages=$(( ($bytes+$pageSize-1) / $pageSize ))
-suffixLength=$(log 10 $pages)
+chunks=$(( ($bytes+$serverChunkSize-1) / $serverChunkSize ))
+suffixLength="$( log 10 $chunks )"
 suffixLength=$(( $suffixLength + 1 ))
 
 # split the database in chunks, and compress each chunk individually
 # the files will be called:
-# $outdir/db.sqlite3.[0-9]{$suffixLength}
-# $outdir/db.sqlite3.[0-9]{$suffixLength}.gz
-# $outdir/db.sqlite3.[0-9]{$suffixLength}.br
+# $outdir/db.sqlite3.$suffix
+# $outdir/db.sqlite3.$suffix.gz
+# $outdir/db.sqlite3.$suffix.br
 # This is useful e.g. with the gzip_static and brotli_static directives in nginx
 # https://nginx.org/en/docs/http/ngx_http_gzip_static_module.html#gzip_static
 # https://github.com/google/ngx_brotli#brotli_static
