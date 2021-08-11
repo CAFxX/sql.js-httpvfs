9a858617bff99b141abfdf6f3937c56041981387
Carlo Alberto Ferraris <cafxx@strayorange.com>
2021-08-11 11:01:38 +0900

    Support compression in create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -2,17 +2,38 @@ set -eu
 
 indb="$1"
 outdir="$2"
+pagesPerServerChunk=${pagesPerServerChunk:-1}
+cpu=$(getconf _NPROCESSORS_ONLN)
+
+find "$outdir" -type f -name 'db.sqlite3.*' -delete
+
+# http://phodd.net/gnu-bc/bcfaq.html#bashlog
+log(){ local x=$1 n=2 l=-1;if [ "$2" != "" ];then n=$x;x=$2;fi;while((x));do let l+=1 x/=n;done;echo $l; }
 
 # for chunked mode, we need to know the database size in bytes beforehand
 bytes="$(stat --printf="%s" "$indb")"
-# set chunk size to 10MiB (needs to be a multiple of the `pragma page_size`!)
-serverChunkSize=$((10 * 1024 * 1024))
-suffixLength=3
-rm -f "$outdir/db.sqlite3"*
-split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
-
 # set request chunk size to match page size
-requestChunkSize="$(sqlite3 "$indb" 'pragma page_size')"
+pageSize="$(sqlite3 "$indb" 'pragma page_size')"
+requestChunkSize=$pageSize
+# set chunk size to a multiple of the `pragma page_size`)
+pagesPerServerChunk=1
+serverChunkSize=$(($pagesPerServerChunk * $pageSize))
+# calculate suffix size
+pages=$(( ($bytes+$pageSize-1) / $pageSize ))
+suffixLength=$(log 10 $pages)
+suffixLength=$(( $suffixLength + 1 ))
+
+# split the database in chunks, and compress each chunk individually
+# the files will be called:
+# $outdir/db.sqlite3.[0-9]{$suffixLength}
+# $outdir/db.sqlite3.[0-9]{$suffixLength}.gz
+# $outdir/db.sqlite3.[0-9]{$suffixLength}.br
+# This is useful e.g. with the gzip_static and brotli_static directives in nginx
+# https://nginx.org/en/docs/http/ngx_http_gzip_static_module.html#gzip_static
+# https://github.com/google/ngx_brotli#brotli_static
+split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
+find "$outdir" -type f -regex 'db\.sqlite3\.[0-9]+$' | xargs -P$cpu -I{} sh -c 'f={}; gzip --keep --best "$f"; brotli --keep --best "$f"'
+
 
 # write a json config
 echo '
